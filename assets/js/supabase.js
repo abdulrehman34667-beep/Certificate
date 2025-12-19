e9e37139b13ccc87da3faf6973a892ac9b904492
abdulrehman34667-beep <abdulrehman34667@gmail.com>
2025-12-19 21:24:29 +0500

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -2,4 +2,4 @@ const supabaseUrl = 'https://levjcxfmvvtcservhngh.supabase.co';
 const supabaseKey = 'sb_publishable_6tc6HSB51vDM4hJc2CLEhA_RMsjHtWt';
 
 const { createClient } = window.supabase;
-const supabase = createClient(supabaseUrl, supabaseKey);
+window.supabaseClient = createClient(supabaseUrl, supabaseKey);
