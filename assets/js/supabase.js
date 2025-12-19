892fd1283f92db8716964d24370331613f3b0fa6
abdulrehman34667-beep <abdulrehman34667@gmail.com>
2025-12-19 21:06:29 +0500

    Update Supabase URL and key in supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'PASTE_YOUR_PROJECT_URL';
-const supabaseKey = 'PASTE_YOUR_ANON_PUBLIC_KEY';
+const supabaseUrl = 'https://levjcxfmvvtcservhngh.supabase.co';
+const supabaseKey = 'sb_publishable_6tc6HSB51vDM4hJc2CLEhA_RMsjHtWt';
 
 const supabase = window.supabase.createClient(
   supabaseUrl,
